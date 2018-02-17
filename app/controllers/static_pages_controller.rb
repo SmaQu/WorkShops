c9fa9a0d36ab278e2a5e307ce91317675b90e79b
SmaQu <maciej.smolik95@gmail.com>
2018-02-17 16:56:53 +0100

    Initialize

---
+++ app/controllers/static_pages_controller.rb
@@ -0,0 +1,9 @@
+class StaticPagesController < ApplicationController
+
+    def contact; end
+
+    def aboute_us; end
+
+    def mission; end
+    
+end
---
+++ config/routes.rb
@@ -0,0 +1,8 @@
+Rails.application.routes.draw do
+  get '/test', to: proc { |el| [200, {},["ok"]]}
+
+  get '/users', to: "users#index"
+  get '/contact', to: "static_pages#contact"
+  get '/about', to: "static_pages#about_us"
+  get '/mission', to: "static_pages#mission"
+end
