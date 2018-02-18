c87547ec8867d5141fb3aed9afc70918b962c4a2
SmaQu <maciej.smolik95@gmail.com>
2018-02-18 10:23:53 +0100

    Add default seeds for users

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,6 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+users = %w{Kasia Maciek Jan Patryk Slawek Mirek Adam Zbyszek Brajan Andzela Szef}
+users.each {|x| User.create(name: x) }
